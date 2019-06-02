41bde7e0f1b6cab8c208917d3b8a3e8654b4d0b5
Giancarlo Franchini <gc@digistro.com>
2019-06-01 21:24:02 -0400

    passing in props to the components

--- src/index.js
+++ src/index.js
@@ -4,11 +4,27 @@ import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
+  // Importing CommentDetail.js by pulling it in as a jsx component //
   return (
     <div className="ui container comments">
-      // Importing CommentDetail.js by pulling it in as a jsx component
-      <CommentDetail />
-
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 3:30PM"
+        content="Cool post!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 5:00PM"
+        content="Eh, I disagree"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="GC"
+        timeAgo="Yesterday at 2:00AM"
+        content="🐊"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
